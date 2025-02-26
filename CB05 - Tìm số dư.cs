03be490befd9949853eb1f01eb557743b5c5b01a
NDuong <139540304+nduong18@users.noreply.github.com>
2025-02-26 20:03:08 +0700

    Create CB05 - Tìm số dư.cs

---
+++ CB05 - Tìm số dư.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Numerics;
+using System.Linq;
+
+namespace C2501_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+
+            int a = numbers[0], b = numbers[1];
+            Console.WriteLine(a%b);
+        }
+    }
+}
